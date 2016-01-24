727bb45dd1ce90b9b31d325ba801438881c6ebe9
Konstantinos Psimoulis <kosta@noima.com>
2016-01-24 07:58:42 -0500

    Starter branch that reads the images

--- app/src/main/java/com/example/danyal/cameraintentactivity/CameraIntentActivity.java
+++ app/src/main/java/com/example/danyal/cameraintentactivity/CameraIntentActivity.java
@@ -1,111 +1,150 @@
 package com.example.danyal.cameraintentactivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.speech.tts.TextToSpeech;
-import android.speech.tts.TextToSpeech.OnInitListener;
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.ImageView;
-import java.text.SimpleDateFormat;
-import android.widget.Toast;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
+import android.widget.TextView;
 
 import com.clarifai.api.ClarifaiClient;
 import com.clarifai.api.RecognitionRequest;
 import com.clarifai.api.RecognitionResult;
 import com.clarifai.api.Tag;
+import com.clarifai.api.exception.ClarifaiException;
 
-public class CameraIntentActivity extends AppCompatActivity {
-    private static final int ACTIVITY_START_CAMERA_APP = 5;
-    private ImageView mPhotoCapturedImageView;
-    private String mImageFileLocation = "";
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import static android.provider.MediaStore.Images.Media;
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+/**
+ * A simple Activity that performs recognition using the Clarifai API.
+ */
+public class CameraIntentActivity extends Activity {
+    private static final String TAG = CameraIntentActivity.class.getSimpleName();
+
+    // IMPORTANT NOTE: you should replace these keys with your own App ID and secret.
+    // These can be obtained at https://developer.clarifai.com/applications
+    private static final String APP_ID = "KUcty1hKE6k6uXtYhaiSHmKIR4ZuMwqaHsFgsWw4";
+    private static final String APP_SECRET = "nHeFdnfr5hZXM-R4MytzRZqXf1Of_ZzMdRmSZIRB";
+
+    private static final int CODE_PICK = 1;
+
+    private final ClarifaiClient client = new ClarifaiClient(APP_ID, APP_SECRET);
+    private Button selectButton;
+    private ImageView imageView;
+    private TextView textView;
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_camera_intent);
-        mPhotoCapturedImageView = (ImageView) findViewById(R.id.capturePhotoImageView);
+        setContentView(R.layout.activity_recognition);
+        imageView = (ImageView) findViewById(R.id.image_view);
+        textView = (TextView) findViewById(R.id.text_view);
+        selectButton = (Button) findViewById(R.id.select_button);
+        selectButton.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                // Send an intent to launch the media picker.
+                final Intent intent = new Intent(Intent.ACTION_PICK, Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, CODE_PICK);
+            }
+        });
     }
 
-    // Commit test
-    public void takePicture(View view) {
-     //   Toast.makeText(this, "camera button pressed", Toast.LENGTH_SHORT).show();
-        Intent callCameraApplicationIntent = new Intent();
-        callCameraApplicationIntent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+    @Override protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (requestCode == CODE_PICK && resultCode == RESULT_OK) {
+            // The user picked an image. Send it to Clarifai for recognition.
+            Log.d(TAG, "User picked image: " + intent.getData());
+            Bitmap bitmap = loadBitmapFromUri(intent.getData());
+            if (bitmap != null) {
+                imageView.setImageBitmap(bitmap);
+                textView.setText("Recognizing...");
+                selectButton.setEnabled(false);
 
-        File photoFile = null;
+                // Run recognition on a background thread since it makes a network call.
+                new AsyncTask<Bitmap, Void, RecognitionResult>() {
+                    @Override protected RecognitionResult doInBackground(Bitmap... bitmaps) {
+                        return recognizeBitmap(bitmaps[0]);
+                    }
+                    @Override protected void onPostExecute(RecognitionResult result) {
+                        updateUIForResult(result);
+                    }
+                }.execute(bitmap);
+            } else {
+                textView.setText("Unable to load selected image.");
+            }
+        }
+    }
+
+    /** Loads a Bitmap from a content URI returned by the media picker. */
+    private Bitmap loadBitmapFromUri(Uri uri) {
         try {
-            photoFile = createImageFile();
-        }   catch (IOException e) {
-            e.printStackTrace();
+            // The image may be large. Load an image that is sized for display. This follows best
+            // practices from http://developer.android.com/training/displaying-bitmaps/load-bitmap.html
+            BitmapFactory.Options opts = new BitmapFactory.Options();
+            opts.inJustDecodeBounds = true;
+            BitmapFactory.decodeStream(getContentResolver().openInputStream(uri), null, opts);
+            int sampleSize = 1;
+            while (opts.outWidth / (2 * sampleSize) >= imageView.getWidth() &&
+                    opts.outHeight / (2 * sampleSize) >= imageView.getHeight()) {
+                sampleSize *= 2;
+            }
+
+            opts = new BitmapFactory.Options();
+            opts.inSampleSize = sampleSize;
+            return BitmapFactory.decodeStream(getContentResolver().openInputStream(uri), null, opts);
+        } catch (IOException e) {
+            Log.e(TAG, "Error loading image: " + uri, e);
         }
-        callCameraApplicationIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
-        startActivityForResult(callCameraApplicationIntent, ACTIVITY_START_CAMERA_APP);
+        return null;
     }
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(requestCode == ACTIVITY_START_CAMERA_APP && resultCode == RESULT_OK) {
-//            ClarifaiClient clarifai = new ClarifaiClient("KUcty1hKE6k6uXtYhaiSHmKIR4ZuMwqaHsFgsWw4", "nHeFdnfr5hZXM-R4MytzRZqXf1Of_ZzMdRmSZIRB");
-//            List<RecognitionResult> results =
-//                    clarifai.recognize(new RecognitionRequest(new File(mImageFileLocation)));
-//
-//            for (Tag tag : results.get(0).getTags()) {
-//                System.out.println(tag.getName() + ": " + tag.getProbability());
-//            }
-            
+    /** Sends the given bitmap to Clarifai for recognition and returns the result. */
+    private RecognitionResult recognizeBitmap(Bitmap bitmap) {
+        try {
+            // Scale down the image. This step is optional. However, sending large images over the
+            // network is slow and  does not significantly improve recognition performance.
+            Bitmap scaled = Bitmap.createScaledBitmap(bitmap, 320,
+                    320 * bitmap.getHeight() / bitmap.getWidth(), true);
+
+            // Compress the image as a JPEG.
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            scaled.compress(Bitmap.CompressFormat.JPEG, 90, out);
+            byte[] jpeg = out.toByteArray();
+
+            // Send the JPEG to Clarifai and return the result.
+            return client.recognize(new RecognitionRequest(jpeg)).get(0);
+        } catch (ClarifaiException e) {
+            Log.e(TAG, "Clarifai error", e);
+            return null;
         }
     }
 
-    File createImageFile() throws IOException {
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "IMAGE-" + timeStamp;
-        File storageDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(imageFileName, ".jpg", storageDirectory);
-        mImageFileLocation = image.getAbsolutePath();
-//        int x = 1;
-//        while(x < 100)
-//        {
-//            System.out.println(mImageFileLocation);
-//            x++;
-//        }
-
-        return image;
-    }
-
-    void setReducedImageSize() {
-        int x = 0;
-        while(x < 100)
-        {
-            System.out.println(mImageFileLocation);
-            x++;
+    /** Updates the UI by displaying tags for the given result. */
+    private void updateUIForResult(RecognitionResult result) {
+        if (result != null) {
+            if (result.getStatusCode() == RecognitionResult.StatusCode.OK) {
+                // Display the list of tags in the UI.
+                StringBuilder b = new StringBuilder();
+                for (Tag tag : result.getTags()) {
+                    b.append(b.length() > 0 ? ", " : "").append(tag.getName());
+                }
+                textView.setText("Tags:\n" + b);
+            } else {
+                Log.e(TAG, "Clarifai: " + result.getStatusMessage());
+                textView.setText("Sorry, there was an error recognizing your image.");
+            }
+        } else {
+            textView.setText("Sorry, there was an error recognizing your image.");
         }
-//        int targetImageViewWidth = mPhotoCapturedImageView.getWidth();
-//        int targetImageViewHeight = mPhotoCapturedImageView.getHeight();
-//
-//        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-//        bmOptions.inJustDecodeBounds = true;
-//        BitmapFactory.decodeFile(mImageFileLocation, bmOptions);
-//        int cameraImageWidth = bmOptions.outWidth;
-//        int cameraImageHeight = bmOptions.outHeight;
-//        int scaleFactor = Math.min(cameraImageWidth/targetImageViewWidth, cameraImageHeight/targetImageViewHeight);
-//        bmOptions.inSampleSize = scaleFactor;
-//        bmOptions.inJustDecodeBounds = false;
-//        Bitmap photoReducedSizeBitmap = BitmapFactory.decodeFile(mImageFileLocation, bmOptions);
-//        mPhotoCapturedImageView.setImageBitmap(photoReducedSizeBitmap);
+        selectButton.setEnabled(true);
     }
-
-
 }
